b536a739f80f62a4be045c605f4617af761da3ec
Shaikh Ahmed Azad <105549990+ShaikhAhmedAzad@users.noreply.github.com>
2026-02-06 09:06:16 +0300

    Delete supabase directory

--- supabase/migrations/20260127000419_40001484-5320-4f3b-be78-fbeed36c140f.sql
+++
@@ -1,9 +0,0 @@
--- Make the videos bucket public so videos can be accessed on the website
-UPDATE storage.buckets SET public = true WHERE id = 'videos';
-
--- Allow anyone to view videos (public read access)
-CREATE POLICY "Public can view videos"
-ON storage.objects
-FOR SELECT
-TO public
-USING (bucket_id = 'videos');
